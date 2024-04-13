bd1a89bb220ee3b8aa294ea820815e2abfa6ceae
Iulian Catrinescu <iulian.catrinescu@gmail.com>
2024-04-13 19:37:43 +0200

    configured a differen base url based on enviroment. This to ensure the possibility of deploying on github pages

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  const env = loadEnv(mode, process.cwd());
+  const baseUrl = env.VITE_BASE;
+  return defineConfig({
+    plugins: [react()],
+    base: baseUrl
+  })
+};
